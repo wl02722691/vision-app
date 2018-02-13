4640c8fd3fb0245417386cb7d3ad1ed60cbac37d
“Alice” <wl02722691@gmail.com>
2018-02-13 20:01:11 +0800

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by 張書涵 on 2018/2/13.
+//  Copyright © 2018年 AliceChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
